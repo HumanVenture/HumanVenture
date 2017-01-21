86553023bd5356fef1971b155b058ccd4d2f0aa9
deathyon <maninblacksweater@gmail.com>
2017-01-22 00:06:18 +0300

    Fix metrics block

--- template-parts/home-metrics.php
+++ template-parts/home-metrics.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/template-parts/home-metrics.css">
+<!-- <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/template-parts/home-metrics.css">
 <div class="metrics">
 	<div class="row">
 		<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
@@ -39,6 +39,54 @@
 					<p class="text-center">286</p>
 					<p class="text-center"><?php echo __('Количество участников', 'hv-serikoff'); ?></p>
 				</div>
+			</div>
+		</div>
+	</div>
+</div>
+ -->
+
+<div class="metrics">
+	<div class="metrics__item">
+		<div class="metrics__thumbnail">
+			<div class="metrics__caption">
+				<p class="metrics__caption-number" style="color: #b1c6d0;">2</p>
+				<p class="metrics__caption-label"><?php echo __('Количество проектов', 'hv-serikoff'); ?></p>
+			</div>
+		</div>
+	</div>
+	
+	<div class="metrics__item">
+		<div class="metrics__thumbnail">
+			<div class="metrics__caption">
+				<p class="metrics__caption-number" style="color: #d0cfcf;">1</p>
+				<p class="metrics__caption-label"><?php echo __('Создано рабочих мест', 'hv-serikoff'); ?></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="metrics__item metrics__item--margin">
+		<div class="metrics__thumbnail">
+			<div class="metrics__caption">
+				<p class="metrics__caption-number" style="color: #86a5b4;">1500</p>
+				<p class="metrics__caption-label"><?php echo __('Количество денег, инвестированных в проекты (руб.)', 'hv-serikoff'); ?></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="metrics__item metrics__item--margin">
+		<div class="metrics__thumbnail">
+			<div class="metrics__caption">
+				<p class="metrics__caption-number" style="color: #b1c6d0;">10</p>
+				<p class="metrics__caption-label"><?php echo __('Запущено возобновляемых источников энергии (Вт)', 'hv-serikoff'); ?></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="metrics__item">
+		<div class="metrics__thumbnail">
+			<div class="metrics__caption">
+				<p class="metrics__caption-number" style="color: #b1c6d0;">286</p>
+				<p class="metrics__caption-label"><?php echo __('Количество участников', 'hv-serikoff'); ?></p>
 			</div>
 		</div>
 	</div>
