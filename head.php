9e50ac6be7ce64437aec7f2d0629f4098eac03d9
deathyon <maninblacksweater@gmail.com>
2017-01-22 15:57:29 +0300

    Specify charset

--- head.php
+++ head.php
@@ -2,6 +2,7 @@
 	<title>HumanVenture</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8" />
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.slim.min.js"></script>
 
