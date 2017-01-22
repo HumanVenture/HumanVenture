11108f246cf44564bef354d5144d5f765fb173e1
deathyon <maninblacksweater@gmail.com>
2017-01-22 13:54:48 +0300

    Improve header view on mobile version

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 	<nav class="navbar navbar-default navbar-static-top" role="navigation">
 		<div class="container">
 			<div class="navbar-header">
-				<div class="row">
+				<div class="row main-header__t-language-switch-row">
 					<div class="col-xs-12">
 						<span class="languages-switcher-buttons">
 							<button class="btn btn-primary btn-xs active language-switcher-button language-switcher-button-en" type="button" onclick="location.href='//en.hv.leinlawun.org';"><?php echo __('en', 'hv-serikoff'); ?></button>
@@ -10,25 +10,25 @@
 						</span>
 					</div>
 				</div>
-				<div class="row row-flex align">
-					<div class="col-xs-8 col-sm-8 col-md-8 col-lg-offset-1 col-lg-7">
+				<div class="row row-flex align main-header__t-logo-row">
+					<div class="col-xs-8 col-sm-8 col-md-8 col-lg-offset-1 col-lg-7 main-header__t-logo">
 						<a class="navbar-brand" href="<?php bloginfo('url'); ?>">
 							<img class="img-responsive" src="<?php bloginfo('template_url'); ?>/logo_white.svg">
 						</a>
 					</div>
-					<div class="col-xs-4 col-sm-4 col-md-4 col-lg-offset-2 col-lg-2">
+					<div class="col-xs-4 col-sm-4 col-md-4 col-lg-offset-2 col-lg-2 main-header__t-login">
 						<span class="login-buttons">
 							<button class="btn btn-primary btn-md active login-button-facebook" type="button" onclick="location.href='<?php bloginfo('url'); ?>/wp-login.php?action=wordpress_social_authenticate&mode=login&provider=Facebook';"><?php echo __('f', 'hv-serikoff'); ?></button>
 							<button class="btn btn-primary btn-md active login-button-vkontakte" type="button" onclick="location.href='<?php bloginfo('url'); ?>/wp-login.php?action=wordpress_social_authenticate&mode=login&provider=Vkontakte';"><?php echo __('в', 'hv-serikoff'); ?></button>
 						</span>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-xs-8 col-sm-8 col-md-8 col-lg-offset-1 col-lg-7">
+				<div class="row main-header__t-description-row">
+					<div class="col-xs-8 col-sm-8 col-md-8 col-lg-offset-1 col-lg-7 main-header__t-description">
 						<p class="text-center tagline tagline-1"><?php echo __('Хотите получить выгоду<br>от решения глобальных проблем человечества?<br>', 'hv-serikoff'); ?></p>
 						<p class="text-center tagline tagline-2"><?php echo __('Присоединяйтесь к нам!<br>', 'hv-serikoff'); ?></p>
 					</div>
-					<div class="col-xs-4 col-sm-4 col-md-4 col-lg-offset-1 col-lg-3">
+					<div class="col-xs-4 col-sm-4 col-md-4 col-lg-offset-1 col-lg-3 main-header__t-user-counter">
 						<p class="text-center info info-1"><?php echo __('Зарегистрированных пользователей', 'hv-serikoff'); ?>: <?php echo count(get_users()); ?></p>
 					</div>
 				</div>
