6e64b5ad4b8c90ea7e7b2f2baa242e3769e9dc63
deathyon <maninblacksweater@gmail.com>
2017-01-28 15:02:44 +0300

    Refactor home page CSS

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
 			<?php get_header(); ?>
 		</div>
 		<div class="container home-wrapper">
-			<?php get_template_part('template-parts/projects', 'content'); ?>
+			<?php get_template_part('template-parts/home', 'content'); ?>
 		</div>
 		<div class="footer-wrapper">
 			<?php get_footer(); ?>
--- template-parts/home-content.php
+++ template-parts/home-content.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/home.css" /> 
+
 <div class="home">
 	<div class="carousel-wrapper">
 		<?php get_template_part('template-parts/home', 'carousel'); ?>
