0ed7f312a012fa249c7860a77b86523348539ead
deathyon <maninblacksweater@gmail.com>
2017-01-28 13:33:19 +0300

    Improve mobile version, add mixins, fix label hooks

--- template-parts/idea-search.php
+++ template-parts/idea-search.php
@@ -15,11 +15,11 @@
       <form class="input-context input-context--theme-basic">
         <div class="input-context__row">
           <div class="input-context__label-wrapper">
-            <label name="search_type" class="input-context__label">Поиск проекта</label>
+            <label for="hook-search-type" class="input-context__label">Поиск проекта</label>
           </div>
 
           <div class="input-context__input-wrapper">
-            <select name="search_type" class="input-context__input input-context__input--select">
+            <select id="hook-search-type" name="search_type" class="input-context__input input-context__input--select">
               <option>По популярности</option>
             </select>
           </div>
@@ -27,11 +27,11 @@
 
         <div class="input-context__row">
           <div class="input-context__label-wrapper">
-            <label name="direction" class="input-context__label">Направление</label>
+            <label for="hook-direction" class="input-context__label">Направление</label>
           </div>
 
           <div class="input-context__input-wrapper">
-            <select name="direction" class="input-context__input input-context__input--select">
+            <select id="hook-direction" name="direction" class="input-context__input input-context__input--select">
               <option>Энергетика</option>
             </select>
           </div>
@@ -39,11 +39,11 @@
 
         <div class="input-context__row">
           <div class="input-context__label-wrapper">
-            <label name="is_commercial" class="input-context__label">Коммерческая идея</label>
+            <label for="hook-is-commercial" class="input-context__label">Коммерческая идея</label>
           </div>
 
           <div class="input-context__input-wrapper">
-            <select name="is_commercial" class="input-context__input input-context__input--select">
+            <select id="hook-is-commercial" name="is_commercial" class="input-context__input input-context__input--select">
               <option>Да</option>
             </select>
           </div>
@@ -51,21 +51,21 @@
 
         <div class="input-context__row">
           <div class="input-context__label-wrapper">
-            <label name="cost" class="input-context__label">Сумма для реализации, USD</label>
+            <label for="hook-cost" class="input-context__label">Сумма для реализации, USD</label>
           </div>
 
           <div class="input-context__input-wrapper">
-            <input name="cost" type="text" class="input-context__input input-context__input--text" />
+            <input id="hook-cost" name="cost" type="text" class="input-context__input input-context__input--text" />
           </div>
         </div>
 
         <div class="input-context__row">
           <div class="input-context__label-wrapper">
-            <label name="deadline" class="input-context__label">Срок реализации</label>
+            <label for="hook-deadline" class="input-context__label">Срок реализации</label>
           </div>
 
           <div class="input-context__input-wrapper">
-            <select name="deadline" class="input-context__input input-context__input--select">
+            <select id="hook-deadline" name="deadline" class="input-context__input input-context__input--select">
               <option>&lt 1 года</option>
             </select>
           </div>
@@ -73,11 +73,11 @@
 
         <div class="input-context__row">
           <div class="input-context__label-wrapper">
-            <label name="scalability" class="input-context__label">Масштабируемость</label>
+            <label for="hook-scalability" class="input-context__label">Масштабируемость</label>
           </div>
 
           <div class="input-context__input-wrapper">
-            <select name="scalability" class="input-context__input input-context__input--select">
+            <select id="hook-scalability" name="scalability" class="input-context__input input-context__input--select">
               <option>нет (локально)</option>
             </select>
           </div>
@@ -85,62 +85,62 @@
 
         <div class="input-context__row">
           <div class="input-context__label-wrapper">
-            <label name="author" class="input-context__label">Автор</label>
+            <label for="hook-author" class="input-context__label">Автор</label>
           </div>
 
           <div class="input-context__input-wrapper">
-            <input name="author" type="text" class="input-context__input input-context__input--text" />
+            <input id="hook-author" name="author" type="text" class="input-context__input input-context__input--text" />
           </div>
         </div>
 
         <div class="input-context__row">
           <div class="input-context__label-wrapper">
-            <label name="created_at" class="input-context__label">Дата создания</label>
+            <label for="hook-created-at" class="input-context__label">Дата создания</label>
           </div>
 
           <div class="input-context__input-wrapper">
-            <input name="created_at" type="text" class="input-context__input input-context__input--text" />
+            <input id="hook-created-at" name="created_at" type="text" class="input-context__input input-context__input--text" />
           </div>
         </div>
 
         <div class="input-context__row">
           <div class="input-context__label-wrapper">
-            <label name="country" class="input-context__label">Страна</label>
+            <label for="hook-country" class="input-context__label">Страна</label>
           </div>
 
           <div class="input-context__input-wrapper">
-            <input name="country" type="text" class="input-context__input input-context__input--text" />
+            <input id="hook-country" name="country" type="text" class="input-context__input input-context__input--text" />
           </div>
         </div>
 
         <div class="input-context__row">
           <div class="input-context__label-wrapper">
-            <label name="posts_number" class="input-context__label">Количество постов</label>
+            <label for="hook-posts-number" class="input-context__label">Количество постов</label>
           </div>
 
           <div class="input-context__input-wrapper">
-            <input name="posts_number" type="text" class="input-context__input input-context__input--text" />
+            <input id="hook-posts-number" name="posts_number" type="text" class="input-context__input input-context__input--text" />
           </div>
         </div>
 
         <div class="input-context__row">
           <div class="input-context__label-wrapper">
-            <label name="rating" class="input-context__label">Рейтинг (голосование)</label>
+            <label for="hook-rating" class="input-context__label">Рейтинг (голосование)</label>
             <p class="input-context__description">(также: рейтинг по Linkedin skills)</p>
           </div>
 
           <div class="input-context__input-wrapper">
-            <input name="rating" type="text" class="input-context__input input-context__input--text" />
+            <input id="hook-rating" name="rating" type="text" class="input-context__input input-context__input--text" />
           </div>
         </div>
 
         <div class="input-context__row">
           <div class="input-context__label-wrapper">
-            <label name="tags" class="input-context__label">Тэги #</label>
+            <label for="hook-tags" class="input-context__label">Тэги #</label>
           </div>
 
           <div class="input-context__input-wrapper">
-            <input name="tags" type="text" class="input-context__input input-context__input--text" />
+            <input id="hook-tags" name="tags" type="text" class="input-context__input input-context__input--text" />
           </div>
         </div>
       </form>
