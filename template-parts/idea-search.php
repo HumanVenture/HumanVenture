a479da5f4e454eb1beb28925be59bf41425ae5cf
deathyon <maninblacksweater@gmail.com>
2017-01-28 13:19:49 +0300

    Add draft for idea cards

--- template-parts/idea-search.php
+++ template-parts/idea-search.php
@@ -8,6 +8,7 @@
   <div class="idea-button">
     <button class="idea-button__button">Есть идея!</button>
   </div>
+  <hr class="idea-search__hr" />
 
   <div class="idea-search__container">
     <div class="idea-search__search">
@@ -146,6 +147,41 @@
     </div>
 
     <div class="idea-search__results">
+      <div class="idea-card">
+        <h1 class="idea-card__category">Медицина</h1>
+        <div  class="idea-card__image"
+              style="background-image: url('<?php bloginfo('template_directory'); ?>/assets/card-medicine.png')">
+        </div>
+        <div class="idea-card__info">
+          <h2 class="idea-card__name">Новый мой</h2>
+          <p class="idea-card__description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          </p>
+          <div class="idea-card__actions">
+            <div class="idea-card__action idea-card__action--reply">
+              <img  class="idea-card__action-icon"
+                    src="<?php bloginfo('template_directory'); ?>/assets/idea-card-action-reply.png"
+              />
+              <p class="idea-card__action-description">ответы</p>
+              <p class="idea-card__action-counter">0</p>
+            </div>
+
+            <div class="idea-card__action idea-card__action--like">
+              <img  class="idea-card__action-icon"
+                    src="<?php bloginfo('template_directory'); ?>/assets/idea-card-action-like.png"
+              />
+              <p class="idea-card__action-description">понравилось</p>
+              <p class="idea-card__action-counter">0</p>
+            </div>
+
+            <div class="idea-card__action idea-card__action--add">
+              <img  class="idea-card__action-icon"
+                    src="<?php bloginfo('template_directory'); ?>/assets/idea-card-action-reply.png"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
