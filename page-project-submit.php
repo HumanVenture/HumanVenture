1b9b94f2dc84abef07047ff87307fe8157ce7c56
Sergey Sherkunov <leinlawun@openmailbox.org>
2017-02-02 09:41:58 +0300

    Fix rendering page project submit

---
+++ page-project-submit.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<?php get_template_part('head'); ?>
+
+	<body>
+		<div class="header-wrapper">
+			<?php get_header(); ?>
+		</div>
+		<div class="container page-project-submit">
+			<?php get_template_part('template-parts/page-project-submit'); ?>
+		</div>
+		<div class="footer-wrapper">
+			<?php get_footer(); ?>
+		</div>
+	</body>
+</html>
